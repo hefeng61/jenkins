331ecb65337630fd853b9bebfa85ed63c058f611
hefeng <aaron@hbtgft.com>
2021-07-07 09:08:31 +0800

    update

---
+++ src/main/java/com/example/jenkins/controller/Hello.java
@@ -0,0 +1,18 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author hefeng
+ * @Description //TODO
+ * @Date 2021/7/7 9:05
+ */
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String sayHi(){
+        return "hello world";
+    }
+}
